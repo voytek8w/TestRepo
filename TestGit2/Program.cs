6a38249f5426cf1402466d599d04a842c17d0de5
voytek8w <wojtek8w@gmail.com>
2018-01-04 14:00:46 +0100

    dodałem Hello Universe

--- TestGit2/Program.cs
+++ TestGit2/Program.cs
@@ -11,6 +11,7 @@ namespace TestGit2
         static void Main(string[] args)
         {
             Console.WriteLine("Czesc");
+            Console.WriteLine("Hello Universe");
         }
     }
 }
